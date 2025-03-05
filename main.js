733ac44561f8ca0ff1d883c2d6a059bc27eb08b4
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2025-03-05 10:10:30 -0400

    Styling the map

--- main.js
+++ main.js
@@ -7,26 +7,41 @@ import VectorLayer from 'ol/layer/Vector';
 
 function init() {
   aylesfordlayers(map);
+
+  let basemap1 = new TileLayer({
+    source: new OSM("OSM_Links.html/OSM_Links.html")
+  });
+  
+  const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM()
+      })
+    ],
+    view: new View({
+      center: fromLonLat([-64.835958, 45.027436]),
+      zoom: 2
+    })
+  });
 }
 
-let basemap1 = new TileLayer({
-  source: new OSM("OSM_Links.html/OSM_Links.html")
-});
-
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
-  view: new View({
-    center: fromLonLat([-64.835958, 45.027436]),
-    zoom: 2
-  })
-});
-
 let aylesfordlayers = (map) => {
+
+  // define style for restaurants
+  let restStyle = new Style({
+    image: new Icon({
+      src: './icons/dining.png',
+      scale: 0.25
+    })
+  });
+
+  let roadStyle = new Style({
+    stroke: new Stroke({
+      color: 'red',
+      width: 5
+    })
+  });
 
   // Define the restaurant layer
   let restFile = './geojsons/map.geojson';
@@ -35,7 +50,8 @@ let aylesfordlayers = (map) => {
     source: new VectorSource({
       url: restFile,
       format: new GeoJSON()
-    })
+    }),
+    style: restStyle
   });
 
   // define the road layer
@@ -45,7 +61,8 @@ let aylesfordlayers = (map) => {
     source: new VectorSource({
       url: roadFile,
       format: new GeoJSON()
-    })
+    }),
+    style: roadStyle
   })
 
   // define the forest layer
